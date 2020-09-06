6173c5aa611367881c56277670ee58c1ad6bdf08
Daniel Muthama <40577986+danielmuthama@users.noreply.github.com>
2020-09-06 15:39:06 -0700

    initial commit

---
+++ hammingcode.py
@@ -0,0 +1,100 @@
+option=int(input('Press 1 for generating hamming code  \nPress 2 for finding error in hamming code\n\t Enter your choice:--\n'))
+
+if(option==1):  # GENERATE HAMMING CODE
+    print('Enter the data bits')
+    d=input()
+    data=list(d)
+    data.reverse()
+    c,ch,j,r,h=0,0,0,0,[]
+
+    while ((len(d)+r+1)>(pow(2,r))):
+        r=r+1
+
+    for i in range(0,(r+len(data))):
+        p=(2**c)
+
+        if(p==(i+1)):
+            h.append(0)
+            c=c+1
+
+        else:
+            h.append(int(data[j]))
+            j=j+1
+
+    for parity in range(0,(len(h))):
+        ph=(2**ch)
+        if(ph==(parity+1)):
+            startIndex=ph-1
+            i=startIndex
+            toXor=[]
+
+            while(i<len(h)):
+                block=h[i:i+ph]
+                toXor.extend(block)
+                i+=2*ph
+
+            for z in range(1,len(toXor)):
+                h[startIndex]=h[startIndex]^toXor[z]
+            ch+=1
+
+    h.reverse()
+    print('Hamming code generated would be:- ', end="")
+    print(int(''.join(map(str, h))))
+
+
+elif(option==2): # DETECT ERROR IN RECEIVED HAMMING CODE
+    print('Enter the hamming code received')
+    d=input()
+    data=list(d)
+    data.reverse()
+    c,ch,j,r,error,h,parity_list,h_copy=0,0,0,0,0,[],[],[]
+
+    for k in range(0,len(data)):
+        p=(2**c)
+        h.append(int(data[k]))
+        h_copy.append(data[k])
+        if(p==(k+1)):
+            c=c+1
+            
+    for parity in range(0,(len(h))):
+        ph=(2**ch)
+        if(ph==(parity+1)):
+
+            startIndex=ph-1
+            i=startIndex
+            toXor=[]
+
+            while(i<len(h)):
+                block=h[i:i+ph]
+                toXor.extend(block)
+                i+=2*ph
+
+            for z in range(1,len(toXor)):
+                h[startIndex]=h[startIndex]^toXor[z]
+            parity_list.append(h[parity])
+            ch+=1
+    parity_list.reverse()
+    error=sum(int(parity_list) * (2 ** i) for i, parity_list in enumerate(parity_list[::-1]))
+    
+    if((error)==0):
+        print('There is no error in the hamming code received')
+
+    elif((error)>=len(h_copy)):
+        print('Error cannot be detected')
+
+    else:
+        print('Error is in',error,'bit')
+
+        if(h_copy[error-1]=='0'):
+            h_copy[error-1]='1'
+
+        elif(h_copy[error-1]=='1'):
+            h_copy[error-1]='0'
+            print('After correction hamming code is:- ')
+        h_copy.reverse()
+        print(int(''.join(map(str, h_copy))))
+
+else:
+    print('Option entered does not exist')
+
+
